9e88564aee86ca58deb1c8a9c6f626f6731f3093
jrandeniya <jrandeniya@gmail.com>
2023-01-27 15:35:38 +1100

    ADDED | Redirect logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,91 +1,68 @@
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from './page.module.css'
+"use client";
 
-const inter = Inter({ subsets: ['latin'] })
+import { useSearchParams } from "next/navigation";
+
+import { useEffect, useState } from "react";
+import styles from "./page.module.css";
+
+let interval: NodeJS.Timer | undefined;
+
+const Redirect = ({ url }: { url: string }) => {
+  const [seconds, setSeconds] = useState(5000);
+
+  useEffect(() => {
+    interval = setInterval(() => {
+      setSeconds((val) => Math.max(val - 1, 0));
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  useEffect(() => {
+    if (seconds === 0) {
+      if (interval) clearInterval(interval);
+      window.location.replace(url);
+    }
+  }, [seconds, url]);
+
+  return (
+    <div style={{ display: "flex", flexDirection: "column", gap: "1rem" }}>
+      <p style={{ textAlign: "center" }}>
+        Redirecting in <span className={styles.code}>{seconds}</span> seconds
+      </p>
+      <small style={{ color: "black", textAlign: "center" }}>{url}</small>
+    </div>
+  );
+};
+
+const getUrlParam = (input: string | null): string | undefined => {
+  if (!input) return undefined;
+
+  if (Array.isArray(input)) return input[0];
+
+  if (input.length === 0) return undefined;
+
+  return input;
+};
 
 export default function Home() {
+  const searchParams = useSearchParams();
+  const url = getUrlParam(searchParams.get("url"));
+  const isValidInput = !!url;
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
       <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
+        <p className={styles.header}>AutoRedirect</p>
+        <div className={styles.description}>
+          {isValidInput && <Redirect url={url} />}
+          {!isValidInput && (
+            <p>
+              You need to pass in <span className={styles.code}>url</span>
+            </p>
+          )}
         </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
       </div>
     </main>
-  )
+  );
 }
